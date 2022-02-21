e4df93c6b06f614a86757adeded4c9cdb9c5f0b7
YazheVika <fuksvik@gmail.com>
2022-02-21 15:16:16 +0200

    added gitignore

---
+++ git2/Program2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
